cefb359790b30f030286604079d2d95325fc82b3
bidard clement <bidard.clement.84@hotmail.com>
2022-03-23 10:17:07 +0100

    railstaskmanager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get '/task/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'task/:id', to: 'tasks#update'
+
+  get '/tasks/new', to: 'tasks#new'
+  post '/tasks', to: 'tasks#create'
+
+  get '/tasks', to: 'tasks#index'
+  get '/task/:id', to: 'tasks#show', as: :task
+
+  delete '/tasks', to: 'tasks#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
